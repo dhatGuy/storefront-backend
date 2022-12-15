737e5f489d69dd1e8de69bed723f9c87ae0207d8
Joseph Odunsi <odunsiolakunbi@gmail.com>
2022-12-15 14:53:38 +0100

    ➕ add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import { mountRoutes } from "./handlers";
 
 const app: Application = express();
 
+const corsOptions = {
+  origin: "*", // TODO: Change this to the domain of the client
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 mountRoutes(app);
